c117c2fc256733d5e9e5810507b29d7838648628
Casey West <casey@geeknest.com>
2015-11-17 19:24:05 -0500

    Enterprise Features™

--- Gemfile
+++ Gemfile
@@ -5,3 +5,6 @@ ruby "2.2.3"
 gem 'puma'
 gem 'sinatra'
 gem 'sinatra-contrib'
+
+gem 'cf-app-utils'
+gem 'redis'
--- app.rb
+++ app.rb
@@ -1,9 +1,44 @@
 require 'sinatra'
 require 'sinatra/contrib'
+require 'cf-app-utils'
+require 'redis'
+require 'singleton'
+
+class Analytics
+  include Singleton
+
+  attr_accessor :redis_connection
+
+  def redis
+    @redis_connection ||= Redis.new(redis_creds)
+  end
+
+  def redis_creds
+    begin
+      creds = CF::App::Credentials
+        .find_all_by_all_service_tags(['redis', 'pivotal'])
+        .first
+      return {
+        host:     creds.fetch('host'),
+        port:     creds.fetch('port'),
+        password: creds.fetch('password')
+      }
+    rescue
+      {}
+    end
+  end
+
+  def visit!
+    redis.incr('visits')
+  end
+end
 
 set :bind, '0.0.0.0'
 set :port, ENV['PORT'] || 4567
 
 get '/' do
-  json instance: ENV['CF_INSTANCE_INDEX']
+  json(
+  	instance: ENV['CF_INSTANCE_INDEX'],
+  	visits:   Analytics.instance.visit!
+  )
 end
